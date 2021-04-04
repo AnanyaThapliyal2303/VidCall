cdf248d3dbb3b02edc752384abb3d129ddabb80b
Ananya <59766849+AnanyaThapliyal2303@users.noreply.github.com>
2021-04-04 14:34:29 +0530

    added 'joined room' code

--- public/script.js
+++ public/script.js
@@ -1,22 +1,24 @@
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
-console.log(videoGrid);
 myVideo.muted=true;
 
-let myVideoStream
 
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(stream=>{
-myVideoStream =stream;
-addVideostream(myVideo, stream);
-})
-
-const addVideostream = (video, stream)=>{
+const addVideoStream = (video, stream)=>{
     video.srcObject =stream;
     video.addEventListener('loadedmetadata',()=>{
         video.play();
     })
     videoGrid.append(video);
 }
+
+let myVideoStream
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream=>{
+myVideoStream =stream;
+addVideoStream(myVideo, stream);
+})
+
+socket.emit('join-room');
